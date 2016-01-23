89f5422d815adefb817f6e11f94dbed770d3cd8d
GBLin <r04943092@ntu.edu.tw>
2016-01-23 20:10:27 +0800

    Facebook login/out allowed now

--- src/components/App.js
+++ src/components/App.js
@@ -1,7 +1,6 @@
 import React, { Component, PropTypes } from 'react';
 import { IndexLink, Link } from 'react-router';
 
-
 const initialState = {
     userThread: 
         [
@@ -43,6 +42,7 @@ const initialState = {
 };
 
 export default class App extends Component {
+
   static propTypes = {
     children: PropTypes.any.isRequired
   }
@@ -50,6 +50,96 @@ export default class App extends Component {
   constructor(props){
     super(props);
     this.state = initialState;
+  }
+
+  componentDidMount() {
+    window.fbAsyncInit = function() {
+      FB.init({
+        appId      : '1640526772891925',
+        cookie     : true,  // enable cookies to allow the server to access
+                          // the session
+        xfbml      : true,  // parse social plugins on this page
+        version    : 'v2.5' // use version 2.5
+      });
+
+      // Now that we've initialized the JavaScript SDK, we call
+      // FB.getLoginStatus().  This function gets the state of the
+      // person visiting this page and can return one of three states to
+      // the callback you provide.  They can be:
+      //
+      // 1. Logged into your app ('connected')
+      // 2. Logged into Facebook, but not your app ('not_authorized')
+      // 3. Not logged into Facebook and can't tell if they are logged into
+      //    your app or not.
+      //
+      // These three cases are handled in the callback function.
+      FB.getLoginStatus(function(response) {
+        this.statusChangeCallback(response);
+      }.bind(this));
+    }.bind(this);
+
+    // Load the SDK asynchronously
+    (function(d, s, id) {
+      var js, fjs = d.getElementsByTagName(s)[0];
+      if (d.getElementById(id)) return;
+      js = d.createElement(s); js.id = id;
+      js.src = "//connect.facebook.net/en_US/sdk.js";
+      fjs.parentNode.insertBefore(js, fjs);
+    }(document, 'script', 'facebook-jssdk'));
+  }
+
+  // Here we run a very simple test of the Graph API after login is
+  // successful.  See statusChangeCallback() for when this call is made.
+  testAPI() {
+    console.log('Welcome!  Fetching your information.... ');
+    FB.api('/me', function(response) {
+    console.log('Successful login for: ' + response.name);
+    console.log('Successful login for: ' + response.id);
+    });
+  }
+
+  // This is called with the results from from FB.getLoginStatus().
+  statusChangeCallback(response) {
+    console.log('statusChangeCallback');
+    console.log(response);
+    // The response object is returned with a status field that lets the
+    // app know the current login status of the person.
+    // Full docs on the response object can be found in the documentation
+    // for FB.getLoginStatus().
+    if (response.status === 'connected') {
+      // Logged into your app and Facebook.
+      this.testAPI();
+    } else if (response.status === 'not_authorized') {
+      // The person is logged into Facebook, but not your app.
+        console.log('Plz log into this app');
+    } else {
+      // The person is not logged into Facebook, so we're not sure if
+      // they are logged into this app or not.
+      console.log('Plz log into Facebook');
+    }
+  }
+
+  // This function is called when someone finishes with the Login
+  // Button.  See the onlogin handler attached to it in the sample
+  // code below.
+  checkLoginState() {
+    FB.getLoginStatus(function(response) {
+      this.statusChangeCallback(response);
+    }.bind(this));
+  }
+
+  handleClick() {
+    let self = this;
+    FB.getLoginStatus(function(response){
+      if(response.status === 'connected'){
+        FB.logout(function(response){
+        console.log('User Logout');
+      });
+      }
+      else{
+        FB.login(self.checkLoginState.bind(self));
+      }
+    });
   }
 
   render() {
@@ -77,6 +167,7 @@ export default class App extends Component {
                 <li><IndexLink to="/" activeClassName="active">Home</IndexLink></li>
                 <li><Link to="/profile" activeClassName="active">Profile</Link></li>
                 <li><Link to="/friends" activeClassName="active">Friends</Link></li>
+                <li><a href='#' onClick={this.handleClick.bind(this)}>Login</a></li>
               </ul>
             </div>
           </div>
@@ -91,3 +182,9 @@ export default class App extends Component {
     );
   }
 }
+
+
+
+
+
+
